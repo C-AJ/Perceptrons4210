43d5a8ebd8676b498f6c4bd5030ffb377944dc24
C-AJ <89857745+C-AJ@users.noreply.github.com>
2023-04-21 00:31:24 -0700

    Hopefully Finalized

--- perceptron.py
+++ perceptron.py
@@ -14,8 +14,8 @@ from sklearn.neural_network import MLPClassifier #pip install scikit-learn==0.18
 import numpy as np
 import pandas as pd
 
-n = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0]
-r = [True, False]
+n = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0] # Learning rate
+r = [True, False] #
 
 df = pd.read_csv('optdigits.tra', sep=',', header=None) #reading the data by using Pandas library
 
@@ -27,14 +27,16 @@ df = pd.read_csv('optdigits.tes', sep=',', header=None) #reading the data by usi
 X_test = np.array(df.values)[:,:64]    #getting the first 64 fields to form the feature data for test
 y_test = np.array(df.values)[:,-1]     #getting the last field to form the class label for test
 
-for : #iterates over n
+Paccuracy = 0
+MLPaccuracy = 0
 
-    for : #iterates over r
+for i in n: #iterates over n
 
+    for j in r: #iterates over r
         #iterates over both algorithms
         #-->add your Pyhton code here
 
-        for : #iterates over the algorithms
+        for k in range(2): #iterates over the algorithms
 
             #Create a Neural Network classifier
             #if Perceptron then
@@ -43,7 +45,11 @@ for : #iterates over n
             #   clf = MLPClassifier() #use those hyperparameters: activation='logistic', learning_rate_init = learning rate, hidden_layer_sizes = number of neurons in the ith hidden layer,
             #                          shuffle = shuffle the training data, max_iter=1000
             #-->add your Pyhton code here
-
+            if k == 0:
+                clf = Perceptron(eta0=i, shuffle=j, max_iter=1000)
+            else:
+                clf = MLPClassifier(activation='logistic', learning_rate_init=i,
+                                    shuffle=j, max_iter=1000)
             #Fit the Neural Network to the training data
             clf.fit(X_training, y_training)
 
@@ -52,14 +58,29 @@ for : #iterates over n
             #for (x_testSample, y_testSample) in zip(X_test, y_test):
             #to make a prediction do: clf.predict([x_testSample])
             #--> add your Python code here
+            numberRight = 0
+            total = 0
+            for (x_testSample, y_testSample) in zip(X_test, y_test):
+                if (clf.predict([x_testSample]) == y_testSample):
+                    numberRight += 1
+                total += 1
 
             #check if the calculated accuracy is higher than the previously one calculated for each classifier. If so, update the highest accuracy
             #and print it together with the network hyperparameters
             #Example: "Highest Perceptron accuracy so far: 0.88, Parameters: learning rate=0.01, shuffle=True"
             #Example: "Highest MLP accuracy so far: 0.90, Parameters: learning rate=0.02, shuffle=False"
             #--> add your Python code here
-
-
+            runAccuracy = numberRight / total
+            if k == 0:
+                if runAccuracy > Paccuracy:
+                    Paccuracy = runAccuracy
+                    print("Highest Perceptron accuracy so far: " + str(Paccuracy) + " learning rate=" + str(i) +
+                          " shuffle=" + str(j))
+            else:
+                if runAccuracy > MLPaccuracy:
+                    MLPaccuracy = runAccuracy
+                    print("Highest MLP accuracy so far: " + str(Paccuracy) + " learning rate=" + str(i) +
+                          " shuffle=" + str(j))
 
 
 
